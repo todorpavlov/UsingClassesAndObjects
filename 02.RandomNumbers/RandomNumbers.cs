ac5adf356e8b08230316f773d2dd3cb153a8b326
Todor Pavlov <todor@recitybg.com>
2015-02-23 11:48:59 +0200

    Second problem

---
+++ 02.RandomNumbers/RandomNumbers.cs
@@ -0,0 +1,23 @@
+﻿//  Problem 2. Random numbers
+
+//  Write a program that generates and prints to the console 10 random values in the range [100, 200].
+
+using System;
+
+    class RandomNumbers
+    {
+        static void Main()
+        {
+            Console.WriteLine("Random Numbers\n");
+
+            Console.WriteLine("10 random numbers within the range [100, 200]:");
+            Random r = new Random();
+            
+            for (int i = 0; i < 10; i++)
+            {
+                int numbers = r.Next(100, 200);
+                Console.Write("{0}, ", numbers);
+            }
+            Console.WriteLine();
+        }
+    }
