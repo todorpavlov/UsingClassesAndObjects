75296d1a253fa1c0fb158a6dc1e4d678b7646d5e
Todor Pavlov <todor@recitybg.com>
2015-02-25 20:09:57 +0200

    Final

--- 05.WorkDays/WorkDays.cs
+++ 05.WorkDays/WorkDays.cs
@@ -4,45 +4,65 @@
 //  Consider that workdays are all days from Monday to Friday except a fixed list of public holidays specified preliminary as array.
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 
 class WorkDays
 {
+
+    static DateTime[] Holidays = {
+                new DateTime(2014,01,08),
+                new DateTime(2014,01,08),
+                new DateTime(2014,01,18),
+                new DateTime(2014,01,22),
+                new DateTime(2014,01,23),    
+                                };
+
     static void Main()
     {
-
-
-        int days = 0; //user input
-
-        int year = 2015;
-        int month = 5;
-        int day = 7;
-
-        DateTime oldDate = new DateTime(year, month, day); //2002, 7, 15
-        DateTime newDate = DateTime.Now;
-
-        // Difference in days, hours, and minutes.
-        TimeSpan ts = oldDate - newDate;
-        // Difference in days.
-        int differenceInDays = ts.Days;
-
-        Console.WriteLine("Difference in days: {0} ", differenceInDays);
-
-        int weekdays = (differenceInDays / 7) * 5;
-
-        Console.WriteLine("Difference in weekdays: {0} ", weekdays);
-
-
-
-
-        DateTime today = DateTime.Now;
-        DateTime answer = today.AddDays(days);
-        Console.WriteLine("Today: {0}", today);
-        Console.WriteLine("{0} days from today: {1}", differenceInDays, answer);
+        DateTime dateNow = DateTime.Now;
+        Console.WriteLine("Enter day in format YEAR/MONTH/DAY:");
+        string[] dateStr = Console.ReadLine().Split('/');
+        int year = Int32.Parse(dateStr[0]);
+        int days = Int32.Parse(dateStr[2]);
+        int month = Int32.Parse(dateStr[1]);
+        DateTime endDate = new DateTime(year, month, days);
+        Console.Write("There are {0} working days between {1} and {2}", WorkingDays(dateNow, endDate), dateNow, endDate);
     }
 
 
-    
-        
-    
+    static int WorkingDays(DateTime dateNow, DateTime dateInFuture)
+    {
+        int daysLenght = Math.Abs((dateNow - dateInFuture).Days);
+        int counter = daysLenght;
+        if (dateNow > dateInFuture)
+        {
+            dateNow = dateInFuture;
+            dateInFuture = DateTime.Now;
+        }
+
+        for (int i = 0; i < daysLenght; i++)
+        {
+            dateNow = dateNow.AddDays(1);
+
+            if (dateNow.DayOfWeek == DayOfWeek.Sunday || dateNow.DayOfWeek == DayOfWeek.Saturday)
+            {
+                counter--;
+            }
+
+            for (int days = 0; days < Holidays.Length; days++)
+            {
+                if (dateNow.CompareTo(Holidays[days]) == 0)
+                {
+                    counter--;
+                }
+            }
+
+        }
+        return counter;
+    }
+
 }
-    
